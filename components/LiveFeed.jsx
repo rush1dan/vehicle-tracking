58d4d5d6336158eb2f0061ed187e63f1d8125add
rush1dan <rushidan10@gmail.com>
2023-09-19 01:26:12 +0600

    Live feed tabs WIP

--- components/LiveFeed.jsx
+++ components/LiveFeed.jsx
@@ -5,16 +5,33 @@ import LiveFeedCard from './LiveFeedCard'
 const LiveFeed = (props) => {
     return (
         <div className={props.className}>
+            <Tabs className={'w-full h-24'} />
+            <CardList className={'w-full h-[calc(100%-6rem)]'} />
+        </div>
+    )
+}
+
+const Tabs = (props) => {
+    return (
+        <div className={props.className}>
+
+        </div>
+    )
+}
+
+const CardList = (props) => {
+    return (
+        <div className={props.className}>
             <div className='w-full h-full p-4 flex flex-col items-start gap-y-4 bg-slate-200 overflow-y-auto'>
-                {
-                    vehicle_data.map((data, index) => {
-                        return (
-                            <div key={data.id} className='w-full'>
-                                <LiveFeedCard index={index} className={'w-full h-20'} data={data} />
-                            </div>
-                        )
-                    })
-                }
+            {
+                vehicle_data.map((data, index) => {
+                    return (
+                        <div key={data.id} className='w-full'>
+                            <LiveFeedCard index={index} className={'w-full h-20'} data={data} />
+                        </div>
+                    )
+                })
+            }
             </div>
         </div>
     )
