5af67cfb0d40ad78510ccc34eaac1cf80e0aa0f5
rush1dan <rushidan10@gmail.com>
2023-09-18 22:21:27 +0600

    Redux configured and in the proper next js way to preserve SSR

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Topbar from '@/components/Topbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Sidebar from '@/components/Sidebar'
+import ReduxProvider from '@/components/ReduxProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,15 +15,17 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				<Topbar className={'w-screen h-16'} />
-				<div className='h-[calc(100vh-4rem)] w-full bg-red-200 overflow-hidden'>
-					<div className='w-full h-full flex flex-row items-center justify-start bg-red-600'>
-						<Sidebar className={'w-32 h-full'} />
-						<div className='w-[calc(100%-8rem)] h-full bg-violet-600/50'>
-							{children}
+				<ReduxProvider>
+					<Topbar className={'w-screen h-16'} />
+					<div className='h-[calc(100vh-4rem)] w-full bg-red-200 overflow-hidden'>
+						<div className='w-full h-full flex flex-row items-center justify-start bg-red-600'>
+							<Sidebar className={'w-32 h-full'} />
+							<div className='w-[calc(100%-8rem)] h-full bg-violet-600/50'>
+								{children}
+							</div>
 						</div>
-					</div>	
-				</div>
+					</div>
+				</ReduxProvider>
 			</body>
 		</html>
 	)
