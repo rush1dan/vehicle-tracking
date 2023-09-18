eed42dc45dd1352981adb6dd0a4dc208117398ce
rush1dan <rushidan10@gmail.com>
2023-09-18 21:11:40 +0600

    Reverse geocoding switched off for now

--- components/Address.jsx
+++ components/Address.jsx
@@ -5,9 +5,13 @@ import axios from 'axios';
 
 const default_address = "Dhaka, Bangladesh";
 
-async function getLocation(lat, lon) {
+async function getReverseGeoCoding(lat, lon) {
     try {
-        const response = await axios.get(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json&zoom=18`);
+        const response = await axios.get(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json&zoom=18`, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.31'
+            }
+        });
         const address = response.data.display_name;
         return address;
     } catch (error) {
@@ -17,14 +21,14 @@ async function getLocation(lat, lon) {
 }
 
 const Address = (props) => {
-    const [address, setAddress] = useState(default_address);
-    useEffect(async () => {
-        const location = await getLocation(props.data.lat, props.data.lon);
-        setAddress(location);
-    }, [props.data])
+    // const [address, setAddress] = useState(default_address);
+    // useEffect(async () => {
+    //     const location = await getReverseGeoCoding(props.data.lat, props.data.lon);
+    //     setAddress(location);
+    // }, [props.data]);
     return (
         <p className={props.className}>
-            {address}
+            Latitude: {props.data.lat}, Longitude: {props.data.lon}
         </p>
     )
 }
