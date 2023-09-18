5774564d3825004dece50deaf3b6be4d28634f8d
rush1dan <rushidan10@gmail.com>
2023-09-18 18:03:41 +0600

    Dynamic marker working

--- components/LeafletMap.jsx
+++ components/LeafletMap.jsx
@@ -4,9 +4,16 @@ import React, { useEffect, useState } from 'react'
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import "leaflet/dist/leaflet.css";
 import L from 'leaflet';
-const myIcon = new L.Icon({
- iconUrl: '/marker_red.png',
- iconSize: [32,32]
+import vehicle_data from '@/demo/vehicles';
+
+const myRedIcon = new L.Icon({
+    iconUrl: '/marker_red.png',
+    iconSize: [32, 32]
+});
+
+const myGreenIcon = new L.Icon({
+    iconUrl: '/marker_green.png',
+    iconSize: [32, 32]
 })
 
 const dhakaLatLang = [23.762, 90.3899];
@@ -17,9 +24,11 @@ const MapControls = (props) => {
     return null;
 }
 
+const positions = [];
+
 const LeafletMap = (props) => {
     const [isMounted, setIsMounted] = useState(false);
-    const [position, setPosition] = useState([51.505, -0.09]);
+    const [vehicles, setVehicles] = useState([]);
     useEffect(() => {
         setIsMounted(true);
     }, []);
@@ -28,20 +37,35 @@ const LeafletMap = (props) => {
     }
     return (
         <div className={`${props.className} relative`}>
-            <MapContainer center={position} zoom={13} scrollWheelZoom={true} className='w-full h-full'>
+            <MapContainer center={dhakaLatLang} zoom={13} scrollWheelZoom={true} className='w-full h-full'>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position} icon={myIcon} >
+                {
+                    vehicles.map((vehicle, index) => {
+                        return (
+                            <Marker key={index} position={[vehicle.lat, vehicle.lon]} icon={vehicle.status == 'moving' ? myGreenIcon : myRedIcon} >
+                                <Popup>
+                                    A pretty CSS3 popup. <br /> Easily customizable.
+                                </Popup>
+                            </Marker>
+                        )
+                    })
+                }
+                {/* <Marker position={position} icon={myIcon} >
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
-                </Marker>
-                <MapControls position={position} />
+                </Marker> */}
+                {/* <MapControls position={position} /> */}
             </MapContainer>
-            <button onClick={() => setPosition(dhakaLatLang)} className='px-4 py-2 bg-slate-500 text-white absolute z-[10000] bottom-10 right-10'>
-                Dhaka
+            <button onClick={() => setVehicles(
+                (current) => {
+                    return [...current, vehicle_data[current.length]];
+                }
+            )} className='px-4 py-2 bg-slate-500 text-white absolute z-[10000] bottom-10 right-10'>
+                Add
             </button>
         </div>
     )
