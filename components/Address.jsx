a8fc28e24f5e05f0f24b612f17a92549e476f4aa
rush1dan <rushidan10@gmail.com>
2023-09-19 01:07:26 +0600

    Formatted reverse geo-location data

--- components/Address.jsx
+++ components/Address.jsx
@@ -7,12 +7,19 @@ const default_address = "Dhaka, Bangladesh";
 
 async function getReverseGeoCoding(lat, lon) {
     try {
-        const response = await axios.get(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json&zoom=18`, {
+        const response = await axios.get(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=geocodejson&zoom=18`, {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.31'
             }
         });
-        const address = response.data.display_name;
+        
+        const geo_data = response.data.features[0].properties.geocoding;
+        const address = `${geo_data.name ? geo_data.name + ', ' : ''}
+        ${geo_data.street ? geo_data.street + ', ' : ''}
+        ${geo_data.locality ? geo_data.locality + ', ' : ''}
+        ${geo_data.district ? geo_data.district + ', ' : ''}
+        ${geo_data.city ? geo_data.city : ''}`;
+        
         return address;
     } catch (error) {
         console.log("Error: " + error.message);
