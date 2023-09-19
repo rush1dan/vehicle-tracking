a5c0d12ab060627aa27503cb1bb4edf53337aac7
rush1dan <rushidan10@gmail.com>
2023-09-19 19:19:56 +0600

    Page link added to logo

--- components/Topbar.jsx
+++ components/Topbar.jsx
@@ -1,13 +1,14 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Topbar = (props) => {
     return (
         <div className={props.className}>
             <div className='w-full h-full bg-blue-600 relative'>
-                <div className='w-24 h-20 absolute top-1/2 left-16 -translate-y-1/2'>
+                <Link href='/' className='w-24 h-20 absolute top-1/2 left-16 -translate-y-1/2'>
                     <Image src='/tracky.png' alt='logo' loading='eager' fill objectFit='contain' />
-                </div>
+                </Link>
             </div>
         </div>
     )
