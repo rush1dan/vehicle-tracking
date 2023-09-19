eb9d99e7a957882b4fc93f539dcd94ff99560d60
rush1dan <rushidan10@gmail.com>
2023-09-19 11:09:30 +0600

    Tabs UI created

--- components/LiveFeed.jsx
+++ components/LiveFeed.jsx
@@ -5,8 +5,10 @@ import LiveFeedCard from './LiveFeedCard'
 const LiveFeed = (props) => {
     return (
         <div className={props.className}>
-            <Tabs className={'w-full h-24'} />
-            <CardList className={'w-full h-[calc(100%-6rem)]'} />
+            <div className='w-full h-full bg-slate-200'>
+                <Tabs className={'w-full h-24'} />
+                <CardList className={'w-full h-[calc(100%-6rem)]'} />
+            </div>
         </div>
     )
 }
@@ -14,7 +16,28 @@ const LiveFeed = (props) => {
 const Tabs = (props) => {
     return (
         <div className={props.className}>
+            <div className='w-full h-full flex flex-row items-center justify-center'>
+                <Tab className='w-24 rounded-tl-md overflow-clip' title={'All Vehicles'} bgColorClass={'bg-[#465691]'} textColorClass={'text-white'} count={0} />
+                <Tab className='w-24' title={'Moving'} bgColorClass={'bg-green-300'} textColorClass={'text-green-600'} count={0} selected={true} selectedColorClass={'bg-green-600'} />
+                <Tab className='w-24' title={'Idle'} bgColorClass={'bg-red-300'} textColorClass={'text-red-600'} count={0} />
+                <Tab className='w-24 rounded-tr-md overflow-clip' title={'Selected'} bgColorClass={'bg-blue-300'} textColorClass={'text-blue-600'} count={0} />
+            </div>
+        </div>
+    )
+}
 
+const Tab = ({ className, title, count, bgColorClass, textColorClass, selected, selectedColorClass }) => {
+    return (
+        <div className={className}>
+            <div className='relative cursor-pointer'>
+                <div className={`py-2 flex flex-col items-center justify-between ${bgColorClass}`}>
+                    <p className={`text-xl font-bold ${textColorClass}`}>{count}</p>
+                    <p className={`text-xs font-semibold ${textColorClass}`}>{title}</p>
+                </div>
+            {
+                selected && <div className={`w-full h-2 z-10 ${selectedColorClass} absolute bottom-0`} />
+            }
+            </div>
         </div>
     )
 }
@@ -22,16 +45,16 @@ const Tabs = (props) => {
 const CardList = (props) => {
     return (
         <div className={props.className}>
-            <div className='w-full h-full p-4 flex flex-col items-start gap-y-4 bg-slate-200 overflow-y-auto'>
-            {
-                vehicle_data.map((data, index) => {
-                    return (
-                        <div key={data.id} className='w-full'>
-                            <LiveFeedCard index={index} className={'w-full h-20'} data={data} />
-                        </div>
-                    )
-                })
-            }
+            <div className='w-full h-full p-4 flex flex-col items-start gap-y-4 overflow-y-auto'>
+                {
+                    vehicle_data.map((data, index) => {
+                        return (
+                            <div key={data.id} className='w-full'>
+                                <LiveFeedCard index={index} className={'w-full h-20'} data={data} />
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     )
