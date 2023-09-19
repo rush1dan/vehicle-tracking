10526db0e6745d2e08e670e75d9d94c64fa804c4
rush1dan <rushidan10@gmail.com>
2023-09-19 11:58:10 +0600

    Vehicle count in live feed tab

--- components/LiveFeed.jsx
+++ components/LiveFeed.jsx
@@ -14,12 +14,15 @@ const LiveFeed = (props) => {
 }
 
 const Tabs = (props) => {
+    const allVehicles = vehicle_data.length;
+    const movingVehicles = vehicle_data.filter((vehicle) => vehicle.status === 'moving').length;
+    const idleVehicles = vehicle_data.filter((vehicle) => vehicle.status === 'idle').length;
     return (
         <div className={props.className}>
             <div className='w-full h-full flex flex-row items-center justify-center'>
-                <Tab className='w-24 rounded-tl-md overflow-clip' title={'All Vehicles'} bgColorClass={'bg-[#465691]'} textColorClass={'text-white'} count={0} />
-                <Tab className='w-24' title={'Moving'} bgColorClass={'bg-green-300'} textColorClass={'text-green-600'} count={0} selected={true} selectedColorClass={'bg-green-600'} />
-                <Tab className='w-24' title={'Idle'} bgColorClass={'bg-red-300'} textColorClass={'text-red-600'} count={0} />
+                <Tab className='w-24 rounded-tl-md overflow-clip' title={'All Vehicles'} bgColorClass={'bg-[#465691]'} textColorClass={'text-white'} count={allVehicles} />
+                <Tab className='w-24' title={'Moving'} bgColorClass={'bg-green-300'} textColorClass={'text-green-600'} count={movingVehicles} selected={true} selectedColorClass={'bg-green-600'} />
+                <Tab className='w-24' title={'Idle'} bgColorClass={'bg-red-300'} textColorClass={'text-red-600'} count={idleVehicles} />
                 <Tab className='w-24 rounded-tr-md overflow-clip' title={'Selected'} bgColorClass={'bg-blue-300'} textColorClass={'text-blue-600'} count={0} />
             </div>
         </div>
