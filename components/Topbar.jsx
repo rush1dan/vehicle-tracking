cbdb520263747ae0b4bd03848b8ff7eb6f969882
rush1dan <rushidan10@gmail.com>
2023-09-19 19:05:24 +0600

    Logo added

--- components/Topbar.jsx
+++ components/Topbar.jsx
@@ -1,10 +1,13 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Topbar = (props) => {
     return (
         <div className={props.className}>
-            <div className='w-full h-full bg-blue-600'>
-
+            <div className='w-full h-full bg-blue-600 relative'>
+                <div className='w-24 h-20 absolute top-1/2 left-16 -translate-y-1/2'>
+                    <Image src='/tracky.png' alt='logo' loading='eager' fill objectFit='contain' />
+                </div>
             </div>
         </div>
     )
