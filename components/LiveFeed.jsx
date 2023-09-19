98fe8e790b4f0b2ffd097f6fb25a49b4f801562e
rush1dan <rushidan10@gmail.com>
2023-09-19 13:40:32 +0600

    Appropriate vehicle lists shown in different live feed tabs implemented

--- components/LiveFeed.jsx
+++ components/LiveFeed.jsx
@@ -7,32 +7,42 @@ import { useDispatch, useSelector } from 'react-redux'
 import { selectTab } from '@/redux/selectedTabSlice'
 
 const LiveFeed = (props) => {
+    const selectedTab = useSelector((state) => state.selectedTab);
+    const selectedVehicle = useSelector((state) => state.selectedVehicle);
+
+    const allVehicles = vehicle_data;
+    const movingVehicles = vehicle_data.filter((vehicle) => vehicle.status === 'moving');
+    const idleVehicles = vehicle_data.filter((vehicle) => vehicle.status === 'idle');
+
+    const vehicleList = selectedTab.value === 'All Vehicles' ? allVehicles : (selectedTab.value === 'Moving' ? movingVehicles : idleVehicles);
+
     return (
         <div className={props.className}>
             <div className='w-full h-full'>
-                <Tabs className={'w-full h-24'} />
-                <CardList className={'w-full h-[calc(100%-6rem)]'} vehicles={vehicle_data} />
+                <Tabs className={'w-full h-24'} allVehicleCount={allVehicles.length} movingVehicleCount={movingVehicles.length}
+                    idleVehicleCount={idleVehicles.length} selectedVehicleCount={selectedVehicle.id ? 1 : 0}/>
+                {
+                    selectedTab.value !== 'Selected' && 
+                    <CardList className={'w-full h-[calc(100%-6rem)]'} vehicles={vehicleList} />
+                }
             </div>
         </div>
     )
 }
 
-const Tabs = (props) => {
+const Tabs = ({className, allVehicleCount = 0, movingVehicleCount = 0, idleVehicleCount = 0, selectedVehicleCount = 0}) => {
     const selectedTab = useSelector((state) => state.selectedTab);
 
-    const allVehicles = vehicle_data.length;
-    const movingVehicles = vehicle_data.filter((vehicle) => vehicle.status === 'moving').length;
-    const idleVehicles = vehicle_data.filter((vehicle) => vehicle.status === 'idle').length;
     return (
-        <div className={props.className}>
+        <div className={className}>
             <div className='w-full h-full flex flex-row items-center justify-center'>
-                <Tab className='w-24 rounded-tl-md overflow-clip' title={'All Vehicles'} bgColorClass={'bg-[#465691]'} textColorClass={'text-white'} count={allVehicles}
+                <Tab className='w-24 rounded-tl-md overflow-clip' title={'All Vehicles'} bgColorClass={'bg-[#465691]'} textColorClass={'text-white'} count={allVehicleCount}
                 selected={selectedTab.value == 'All Vehicles'} selectedColorClass={'bg-slate-100'} />
-                <Tab className='w-24' title={'Moving'} bgColorClass={'bg-green-300'} textColorClass={'text-green-600'} count={movingVehicles}
+                <Tab className='w-24' title={'Moving'} bgColorClass={'bg-green-300'} textColorClass={'text-green-600'} count={movingVehicleCount}
                 selected={selectedTab.value == 'Moving'} selectedColorClass={'bg-green-600'} />
-                <Tab className='w-24' title={'Idle'} bgColorClass={'bg-red-300'} textColorClass={'text-red-600'} count={idleVehicles}
+                <Tab className='w-24' title={'Idle'} bgColorClass={'bg-red-300'} textColorClass={'text-red-600'} count={idleVehicleCount}
                 selected={selectedTab.value == 'Idle'} selectedColorClass={'bg-red-600'} />
-                <Tab className='w-24 rounded-tr-md overflow-clip' title={'Selected'} bgColorClass={'bg-blue-300'} textColorClass={'text-blue-600'} count={0}
+                <Tab className='w-24 rounded-tr-md overflow-clip' title={'Selected'} bgColorClass={'bg-blue-300'} textColorClass={'text-blue-600'} count={selectedVehicleCount}
                 selected={selectedTab.value == 'Selected'} selectedColorClass={'bg-blue-600'} />
             </div>
         </div>
