fe9c57a9eeb89dc909aa04d3b3f489edbe6a952c
rush1dan <rushidan10@gmail.com>
2023-09-21 17:21:16 +0600

    Vehicle addition removal UI interactive elements prepared

--- components/AddForm.jsx
+++ components/AddForm.jsx
@@ -1,11 +1,29 @@
+'use client'
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddForm = ({ className, close }) => {
 
+    const [id, setId] = useState('');
+    const [model, setModel] = useState('');
+    const [status, setStatus] = useState('');
+    const [category, setCategory] = useState('');
+    const [lat, setLat] = useState(0);
+    const [lon, setLon] = useState(0);
+
     function handleSubmit(e) {
-        console.log('submit');
         e.preventDefault();
+        const data = {
+            'id': {
+                'id': id,
+                'lat': lat,
+                'lon': lon,
+                'status': status,
+                'category': category,
+                'model': model
+            }
+        }
     }
 
     return (
@@ -19,11 +37,13 @@ const AddForm = ({ className, close }) => {
                     <div className='flex flex-row items-center justify-center gap-x-8'>
                         <div className='flex flex-col items-start justify-between'>
                             <label htmlFor='id' className='font-semibold text-gray-500 px-2 py-1'>Id</label>
-                            <input type='text' id='id' name='id' className='text-base border border-gray-500 rounded-md w-28 p-2' required />
+                            <input type='text' id='id' name='id' className='text-base border border-gray-500 rounded-md w-28 p-2' required
+                                onChange={(e) => setId(e.target.value)}/>
                         </div>
                         <div className='flex flex-col items-start justify-between'>
                             <label htmlFor='model' className='font-semibold text-gray-500 px-2 py-1'>Model</label>
-                            <input type='text' id='model' name='model' className='text-base border border-gray-500 rounded-md w-28 p-2' required />
+                            <input type='text' id='model' name='model' className='text-base border border-gray-500 rounded-md w-28 p-2' required
+                                onChange={(e) => setModel(e.target.value)}/>
                         </div>
                     </div>
 
@@ -32,12 +52,12 @@ const AddForm = ({ className, close }) => {
                         <div className='flex flex-col items-start justify-between'>
                             <label htmlFor='id' className='font-semibold text-gray-500 px-2 py-1'>Status</label>
                             <input type='text' id='id' name='id' className='text-base border border-gray-500 rounded-md w-28 p-2' required
-                                value={'Idle'}/>
+                                value={'Idle'} onChange={(e) => setStatus(e.target.value)}/>
                         </div>
                         <div className='flex flex-col items-start justify-between'>
                             <label htmlFor='model' className='font-semibold text-gray-500 px-2 py-1'>Category</label>
                             <input type='text' id='model' name='model' className='text-base border border-gray-500 rounded-md w-28 p-2' required
-                                value={'Car'}/>
+                                value={'Car'} onChange={(e) => setCategory(e.target.value)}/>
                         </div>
                     </div>
 
@@ -45,11 +65,13 @@ const AddForm = ({ className, close }) => {
                     <div className='flex flex-row items-center justify-center gap-x-8'>
                         <div className='flex flex-col items-start justify-between'>
                             <label htmlFor='lat' className='font-semibold text-gray-500 px-2 py-1'>Latitude</label>
-                            <input type='number' min={-90} max={90} id='lat' name='lat' className='text-base border border-gray-500 rounded-md w-28 p-2' required />
+                            <input type='number' min={-90} max={90} id='lat' name='lat' className='text-base border border-gray-500 rounded-md w-28 p-2' required
+                                onChange={(e) => setLat(e.target.value)}/>
                         </div>
                         <div className='flex flex-col items-start justify-between'>
                             <label htmlFor='lon' className='font-semibold text-gray-500 px-2 py-1'>Longitude</label>
-                            <input type='number' min={-180} max={180} id='lon' name='lon' className='text-base border border-gray-500 rounded-md w-28 p-2' required />
+                            <input type='number' min={-180} max={180} id='lon' name='lon' className='text-base border border-gray-500 rounded-md w-28 p-2' required
+                                onChange={(e) => setLon(e.target.value)}/>
                         </div>
                     </div>
                     <button type='submit' className='mt-8 px-6 py-3 bg-slate-400 hover:bg-slate-500 text-white font-semibold text-lg rounded-lg'>Submit</button>
--- pages/DashboardPage.jsx
+++ pages/DashboardPage.jsx
@@ -100,19 +100,7 @@ const VehiclesList = ({ className, vehicles }) => {
 
 const VehiclesListItem = ({ className, vehicle }) => {
     const dispatch = useDispatch();
-    const selectedVehicle = useSelector((state) => state.selectedVehicle);
-    const [isSelected, setIsSelected] = useState(false);
-    useEffect(() => {
-        if (selectedVehicle.id === vehicle.id) {
-            if (!isSelected) {
-                setIsSelected(true);
-            }
-        }
-        else if (isSelected) {
-            setIsSelected(false);
-        }
-    }, [selectedVehicle]);
-
+    
     let vehicle_pic = '/placeholder_image.svg'
     switch (vehicle.category) {
         case 'car':
@@ -130,25 +118,27 @@ const VehiclesListItem = ({ className, vehicle }) => {
 
     return (
         <div className={className}>
-            <div className={`w-full h-full bg-slate-100 shadow-md shadow-slate-900/10 rounded-md p-2 cursor-pointer hover:ring-2 
-                ${isSelected ? 'ring-4' : ''}`}
+            <div className={`w-full h-full bg-slate-100 shadow-md shadow-slate-900/10 rounded-md p-4`}
                 onClick={() => {
                     dispatch(selectVehicle(vehicle.id));
                 }}>
-                <div className='w-full h-full flex flex-row items-center justify-between gap-x-4'>
-                    <div className='h-3/4 aspect-square rounded-md bg-slate-300 relative'>
+                <div className='w-full h-full flex flex-row items-center justify-between gap-x-8'>
+                    <div className='h-full aspect-square rounded-md bg-slate-300 relative'>
                         <Image src={vehicle_pic} alt='vehicle pic' loading='lazy' fill />
                     </div>
                     <div className='grow flex flex-col justify-between items-start'>
                         <p>{vehicle.model}</p>
                         <p>{vehicle.id}</p>
                         {/* <Address className={'text-xs'} latlondata={{ lat: vehicle.lat, lon: vehicle.lon }} selected={isSelected} /> */}
-                    </div>
-                    <div className='h-full flex flex-col justify-start items-center'>
                         <div className={`w-16 h-6 rounded-md flex justify-center items-center ${vehicle.status == 'moving' ? 'bg-green-300' : 'bg-red-300'}`}>
                             <p className={`${vehicle.status == 'moving' ? 'text-green-600' : 'text-red-600'} 
                             font-semibold text-sm`}>{vehicle.status.charAt(0).toUpperCase() + vehicle.status.slice(1)}</p>
                         </div>
+                    </div>
+                    <div className='h-full flex flex-col justify-center items-center'>
+                        <button className='px-4 py-2 bg-red-500 hover:bg-red-600 text-red-950 rounded-md '>
+                            Remove
+                        </button>
                     </div>
                 </div>
             </div>
