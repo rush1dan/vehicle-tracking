908006d7bb3bb7b9ffeb15c0ac2cd612d3f37cb6
rush1dan <rushidan10@gmail.com>
2023-09-21 16:45:03 +0600

    Add from WIP

--- components/AddForm.jsx
+++ components/AddForm.jsx
@@ -2,66 +2,48 @@ import Image from 'next/image'
 import React from 'react'
 
 const AddForm = ({ className, close }) => {
+
+    function handleSubmit(e) {
+        console.log('submit');
+        e.preventDefault();
+    }
+
     return (
         <div className={className}>
             <div className='w-full h-full p-12 bg-slate-50 rounded-lg shadow-lg shadow-slate-800/20 relative'>
                 <button className='w-8 h-8 absolute top-8 right-8 translate-x-1/2 -translate-y-1/2' onClick={() => close()}>
                     <Image src='/x.svg' alt='cross' fill />
                 </button>
-                <form className="w-full max-w-lg">
-                    <div className="flex flex-wrap -mx-3 mb-6">
-                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
-                                First Name
-                            </label>
-                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
-                                <p className="text-red-500 text-xs italic">Please fill out this field.</p>
+                <form className="w-full h-full flex flex-col items-center justify-start gap-y-4" onSubmit={(e) => handleSubmit(e)}>
+                    {/* Id and Model */}
+                    <div className='flex flex-row items-center justify-center gap-x-8'>
+                        <div className='flex flex-col items-start justify-between'>
+                            <label htmlFor='id' className='font-semibold text-gray-500 px-2 py-1'>Id</label>
+                            <input type='text' id='id' name='id' className='text-base border border-gray-500 rounded-md w-28 p-2' required />
                         </div>
-                        <div className="w-full md:w-1/2 px-3">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-last-name">
-                                Last Name
-                            </label>
-                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
+                        <div className='flex flex-col items-start justify-between'>
+                            <label htmlFor='model' className='font-semibold text-gray-500 px-2 py-1'>Model</label>
+                            <input type='text' id='model' name='model' className='text-base border border-gray-500 rounded-md w-28 p-2' required />
                         </div>
                     </div>
-                    <div className="flex flex-wrap -mx-3 mb-6">
-                        <div className="w-full px-3">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
-                                Password
-                            </label>
-                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
-                                <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+
+                    {/* Status and Category */}
+                    <div className='flex flex-row items-center justify-center gap-x-8'>
+
+                    </div>
+
+                    {/* Lat and Lon */}
+                    <div className='flex flex-row items-center justify-center gap-x-8'>
+                        <div className='flex flex-col items-start justify-between'>
+                            <label htmlFor='lat' className='font-semibold text-gray-500 px-2 py-1'>Latitude</label>
+                            <input type='number' min={-90} max={90} id='lat' name='lat' className='text-base border border-gray-500 rounded-md w-28 p-2' required />
+                        </div>
+                        <div className='flex flex-col items-start justify-between'>
+                            <label htmlFor='lon' className='font-semibold text-gray-500 px-2 py-1'>Longitude</label>
+                            <input type='number' min={-180} max={180} id='lon' name='lon' className='text-base border border-gray-500 rounded-md w-28 p-2' required />
                         </div>
                     </div>
-                    <div className="flex flex-wrap -mx-3 mb-2">
-                        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-city">
-                                City
-                            </label>
-                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque" />
-                        </div>
-                        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
-                                State
-                            </label>
-                            <div className="relative">
-                                <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                                    <option>New Mexico</option>
-                                    <option>Missouri</option>
-                                    <option>Texas</option>
-                                </select>
-                                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-zip">
-                                Zip
-                            </label>
-                            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210" />
-                        </div>
-                    </div>
+                    <button type='submit' className='mt-8 px-6 py-3 bg-slate-400 text-white font-semibold text-lg rounded-lg'>Submit</button>
                 </form>
             </div>
         </div>
