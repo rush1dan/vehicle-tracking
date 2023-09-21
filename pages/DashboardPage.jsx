f3ed6f731014eb5d66da204933d7af8241461049
rush1dan <rushidan10@gmail.com>
2023-09-21 14:30:14 +0600

    Dashboard vehicle list WIP

--- pages/DashboardPage.jsx
+++ pages/DashboardPage.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React, {useState, useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { selectVehicle } from '@/redux/selectedVehicleSlice'
 
 const DashboardPage = () => {
     const selectedVehicle = useSelector((state) => state.selectedVehicle);
@@ -13,12 +14,14 @@ const DashboardPage = () => {
     return (
         <div className='w-full h-full'>
             <div className='w-full h-full flex flex-col items-center justify-start'>
-                <div className='w-full h-full p-4 max-w-7xl'>
-                    <div className='flex flex-row items-center justify-start gap-x-8'>
+                <div className='w-full h-full p-4 max-w-7xl flex flex-col items-center justify-start gap-y-20'>
+                    <div className='w-full flex flex-row items-center justify-start gap-x-8'>
                         <InfoBox className={'w-40 h-24'} status={'All'} vehicleCount={allVehiclesList.length} />
                         <InfoBox className={'w-40 h-24'} status={'Moving'} vehicleCount={movingVehiclesList.length} />
                         <InfoBox className={'w-40 h-24'} status={'Idle'} vehicleCount={idleVehiclesList.length} />
                     </div>
+                    <AddButton />
+                    <VehiclesList className={'w-full'} vehicles={allVehiclesList} />
                 </div>
             </div>
         </div>
@@ -53,6 +56,90 @@ const InfoBox = ({ className, status, vehicleCount }) => {
                     <div className='flex flex-col items-start justify-center'>
                         <p className='font-bold text-lg'>{status}</p>
                         <p className='font-semibold text-xl'>{vehicleCount}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const AddButton = () => {
+    return (
+        <button className='px-8 py-4 rounded-lg text-2xl text-white font-semibold tracking-wider bg-blue-400 hover:bg-blue-500'>
+            ADD
+        </button>
+    )
+}
+
+const VehiclesList = ({ className, vehicles }) => {
+    return (
+        <div className={className}>
+            <div className='w-full h-full px-4 md:py-4 flex flex-col items-start gap-y-4 overflow-y-auto'>
+                {
+                    vehicles.map((vehicle, index) => {
+                        return (
+                            <div key={vehicle.id} className='w-full'>
+                                <VehiclesListItem className={'w-full h-20'} vehicle={vehicle} />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+const VehiclesListItem = ({ className, vehicle }) => {
+    const dispatch = useDispatch();
+    const selectedVehicle = useSelector((state) => state.selectedVehicle);
+    const [isSelected, setIsSelected] = useState(false);
+    useEffect(() => {
+        if (selectedVehicle.id === vehicle.id) {
+            if (!isSelected) {
+                setIsSelected(true);
+            }
+        }
+        else if (isSelected) {
+            setIsSelected(false);
+        }
+    }, [selectedVehicle]);
+
+    let vehicle_pic = '/placeholder_image.svg'
+    switch (vehicle.category) {
+        case 'car':
+            vehicle_pic = '/car.svg';
+            break;
+        case 'bus':
+            vehicle_pic = '/bus.svg';
+            break;
+        case 'truck':
+            vehicle_pic = '/truck.svg';
+            break;
+        default:
+            break;
+    }
+
+    return (
+        <div className={className}>
+            <div className={`w-full h-full bg-slate-100 shadow-md shadow-slate-900/10 rounded-md p-2 cursor-pointer hover:ring-2 
+                ${isSelected ? 'ring-4' : ''}`}
+                onClick={() => {
+                    dispatch(selectVehicle(vehicle.id));
+                }}>
+                <div className='w-full h-full flex flex-row items-center justify-between gap-x-4'>
+                    <div className='h-3/4 aspect-square rounded-md bg-slate-300 relative'>
+                        <Image src={vehicle_pic} alt='vehicle pic' loading='lazy' fill />
+                    </div>
+                    <div className='grow flex flex-col justify-between items-start'>
+                        <p>{vehicle.model}</p>
+                        <p>{vehicle.id}</p>
+                        {/* <Address className={'text-xs'} latlondata={{ lat: vehicle.lat, lon: vehicle.lon }} selected={isSelected} /> */}
+                    </div>
+                    <div className='h-full flex flex-col justify-start items-center'>
+                        <div className={`w-16 h-6 rounded-md flex justify-center items-center ${vehicle.status == 'moving' ? 'bg-green-300' : 'bg-red-300'}`}>
+                            <p className={`${vehicle.status == 'moving' ? 'text-green-600' : 'text-red-600'} 
+                            font-semibold text-sm`}>{vehicle.status.charAt(0).toUpperCase() + vehicle.status.slice(1)}</p>
+                        </div>
                     </div>
                 </div>
             </div>
