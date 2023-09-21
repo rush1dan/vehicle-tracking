4c5f99d0c8cc75de5f46245d0e7884f1e6dc3fd7
rush1dan <rushidan10@gmail.com>
2023-09-21 12:37:47 +0600

    Sidebar width fixed for mobile

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
 					<Topbar className={'w-screen h-16'} />
 					<div className='h-[calc(100vh-4rem)] w-full overflow-hidden'>
 						<div className='w-full h-full flex flex-row items-center justify-start relative'>
-							<Sidebar className={'lg:w-32 md:w-28 h-full'} />
+							<Sidebar className={'lg:w-32 md:w-28 w-32 h-full'} />
 							<div className='lg:w-[calc(100%-8rem)] md:w-[calc(100%-7rem)] w-full h-full'>
 								{children}
 							</div>
