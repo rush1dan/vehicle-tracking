4d2ffab91b6f1a62bbf5b3894daaa3497a5c26ca
rush1dan <rushidan10@gmail.com>
2023-10-02 15:36:15 +0600

    Register page created with proper route and layout

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,7 +1,5 @@
-import Topbar from '@/components/Topbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Sidebar from '@/components/Sidebar'
 import ReduxProvider from '@/components/ReduxProvider'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,15 +14,7 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 			<body className={inter.className}>
 				<ReduxProvider>
-					<Topbar className={'w-screen h-16'} />
-					<div className='h-[calc(100vh-4rem)] w-full overflow-hidden'>
-						<div className='w-full h-full flex flex-row items-center justify-start relative'>
-							<Sidebar className={'lg:w-32 md:w-28 w-32 h-full'} />
-							<div className='lg:w-[calc(100%-8rem)] md:w-[calc(100%-7rem)] w-full h-full'>
-								{children}
-							</div>
-						</div>
-					</div>
+					{children}
 				</ReduxProvider>
 			</body>
 		</html>
--- client/app/page.js
+++ client/app/page.js
@@ -1,10 +1,20 @@
+import Topbar from '@/components/Topbar'
+import Sidebar from '@/components/Sidebar'
 import PageLoader from "@/views/PageLoader"
 
 
 export default function Home() {
 	return (
 		<main className="w-full h-full">
-			<PageLoader className='w-full h-full' />
+			<Topbar className={'w-screen h-16'} />
+			<div className='h-[calc(100vh-4rem)] w-full overflow-hidden'>
+				<div className='w-full h-full flex flex-row items-center justify-start relative'>
+					<Sidebar className={'lg:w-32 md:w-28 w-32 h-full'} />
+					<div className='lg:w-[calc(100%-8rem)] md:w-[calc(100%-7rem)] w-full h-full'>
+						<PageLoader className='w-full h-full' />
+					</div>
+				</div>
+			</div>
 		</main>
 	)
 }
