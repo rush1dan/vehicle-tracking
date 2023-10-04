6ead38b2340fe037fcf93d262952cee163e03355
rush1dan <rushidan10@gmail.com>
2023-10-04 10:49:06 +0600

    Sign out functionality implemented

--- client/components/SignOutModal.jsx
+++ client/components/SignOutModal.jsx
@@ -1,10 +1,17 @@
+import { signOut } from 'next-auth/react'
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
-const SignOutModal = ({className}) => {
+const SignOutModal = ({ className }) => {
+    const router = useRouter();
+    async function handleSignOut() {
+        await signOut({ redirect: false });
+        router.push('/signin');
+    }
     return (
         <div className={className}>
-            <div className='flex flex-col items-center justify-center p-12 bg-sky-100 shadow-slate-700/10 shadow-lg rounded-lg'>
-                <button className='bg-purple-500 rounded-md w-32 h-12 hover:bg-purple-200'>
+            <div className='flex flex-col items-center justify-center p-12 bg-neutral-100 shadow-slate-700/10 shadow-lg rounded-lg'>
+                <button className='bg-purple-500 rounded-md w-32 h-12 hover:bg-purple-200' onClick={e => handleSignOut()}>
                     <span className='text-white text-xl font-semibold text'>Sign Out</span>
                 </button>
             </div>
