99f1da9c4dee865be0316d016a451aa71a9dd8af
rush1dan <rushidan10@gmail.com>
2023-10-05 21:53:27 +0600

    Vehicle card update from updated

--- client/components/ForLivePage/UpdateForm.jsx
+++ client/components/ForLivePage/UpdateForm.jsx
@@ -25,27 +25,27 @@ const UpdateForm = ({ className, vehicle }) => {
     return (
         <div className={className}>
             <div className='w-full flex flex-col items-center justify-start gap-y-2'>
-                <p className='text-xl font-bold underline'>Edit</p>
+                <p className='text-xl text-gray-500 font-bold'>For Demo Data Change:</p>
                 <div className='w-full border-2 border-gray-500 rounded-md flex flex-col items-center justify-start p-4 gap-y-6'>
                     <div className='w-full flex flex-row items-center justify-center gap-x-4'>
                         <p className='w-10 font-semibold text-xl text-gray-500'>Lat:</p>
                         <div className='flex flex-row items-center justify-start'>
-                            <button className='w-10 h-10 bg-white border-2 border-gray-600 text-center text-3xl'
+                            <button className='w-10 h-10 bg-white border-2 border-gray-600 text-center text-3xl rounded-tl-full rounded-bl-full'
                                 onClick={() => updateLatitude(1)}>+</button>
-                            <input type="text" className='w-20 h-10 border-2 border-gray-600 text-center text-lg font-light'
+                            <input type="text" className='w-24 h-10 border-2 border-gray-600 text-center text-base font-light'
                                 value={vehicle.lat.toFixed(4)} readOnly />
-                            <button className='w-10 h-10 bg-white border-2 border-gray-600 text-center text-3xl'
+                            <button className='w-10 h-10 bg-white border-2 border-gray-600 text-center text-3xl rounded-tr-full rounded-br-full'
                                 onClick={() => updateLatitude(-1)}>-</button>
                         </div>
                     </div>
                     <div className='w-full flex flex-row items-center justify-center gap-x-4'>
                         <p className='w-10 font-semibold text-xl text-gray-500'>Lon:</p>
                         <div className='flex flex-row items-center justify-start'>
-                            <button className='w-10 h-10 bg-white border-2 border-gray-600 text-center text-3xl'
+                            <button className='w-10 h-10 bg-white border-2 border-gray-600 text-center text-3xl rounded-tl-full rounded-bl-full'
                                 onClick={() => updateLongitude(1)}>+</button>
-                            <input type="text" className='w-20 h-10 border-2 border-gray-600 text-center text-lg font-light'
+                            <input type="text" className='w-24 h-10 border-2 border-gray-600 text-left text-base font-light'
                                 value={vehicle.lon.toFixed(4)} readOnly />
-                            <button className='w-10 h-10 bg-white border-2 border-gray-600 text-center text-3xl'
+                            <button className='w-10 h-10 bg-white border-2 border-gray-600 text-center text-3xl rounded-tr-full rounded-br-full'
                                 onClick={() => updateLongitude(-1)}>-</button>
                         </div>
                     </div>
