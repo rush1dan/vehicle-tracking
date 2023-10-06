b2f353629f236593e0603c09787cd50bc050e413
rush1dan <rushidan10@gmail.com>
2023-10-06 11:20:45 +0600

    &apos; build error resolved

--- client/app/(auth-client)/signin/page.jsx
+++ client/app/(auth-client)/signin/page.jsx
@@ -139,7 +139,7 @@ export default function SignInPage() {
                     </form>
 
                     <p className="mt-10 text-center text-sm text-gray-500">
-                        Don't have an account?{' '}
+                        Don&apos;t have an account?{' '}
                         <Link href="/register" className="font-semibold leading-6 text-blue-600 hover:text-blue-500">
                             Register
                         </Link>
