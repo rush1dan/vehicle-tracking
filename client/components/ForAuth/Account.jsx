d816306aa80a079595be8854d64eb5d4967203e6
rush1dan <rushidan10@gmail.com>
2023-10-08 20:18:58 +0600

    User account button made uninteractable on live page

--- client/components/ForAuth/Account.jsx
+++ client/components/ForAuth/Account.jsx
@@ -4,11 +4,14 @@ import { useSession } from 'next-auth/react'
 import Image from 'next/image'
 import React, { useState } from 'react'
 import SignOutModal from './SignOutModal'
+import { useSelector } from 'react-redux'
 
 const Account = ({className}) => {
     const { data: session, status } = useSession();
 
     const [showSignOutModal, setShowSignOutModal] = useState(false);
+
+    const selectedPage = useSelector((state) => state.selectedPage);
 
     if (status !== 'authenticated') {
         return null;
@@ -16,8 +19,8 @@ const Account = ({className}) => {
     return (
         <div className={className}>
             <div className='w-full h-full'>
-                <div className='w-full h-full flex flex-row items-center justify-center gap-x-2 rounded-full p-2 bg-blue-300/50 hover:bg-white/50 border-2 border-white
-                cursor-pointer' onClick={e => setShowSignOutModal(!showSignOutModal)}>
+                <div className={`w-full h-full flex flex-row items-center justify-center gap-x-2 rounded-full p-2 bg-blue-300/50  border-2 border-white 
+                ${selectedPage.value === 'Dashboard' ? 'cursor-pointer hover:bg-white/50' : 'pointer-events-none'}`} onClick={e => setShowSignOutModal(!showSignOutModal)}>
                     <div className='w-6 h-6 rounded-full relative'>
                         <Image src='/user.svg' alt='profile' fill />
                     </div>
